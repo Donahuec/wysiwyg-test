3d9bef79527a72bc1a6c27a740f9866dd33a29b8
Caitlin Donahue <CaitlinDonahue95@gmail.com>
2025-01-15 13:03:30 -0600

    Add Snow version

--- index.js
+++ index.js
@@ -20,3 +20,24 @@ quill.on("text-change", (delta, oldDelta, source) => {
   document.querySelector("#out").textContent = html;
   document.querySelector("#outc").textContent = html.replace("&nbsp;", " ");
 });
+
+const quillSnow = new Quill("#quillSnow", {
+  placeholder: "Placeholder",
+  formats: ["bold", "italic", "underline"],
+  modules: {
+    toolbar: toolbarOptions,
+  },
+  theme: "snow",
+});
+
+let htmlSnow = quillSnow.getSemanticHTML(0);
+document.querySelector("#outsnow").textContent = html;
+document.querySelector("#outsnowc").textContent = html.replace("&nbsp;", " ");
+
+quillSnow.on("text-change", (delta, oldDelta, source) => {
+  console.log("delta", delta);
+  console.log("olddelta", oldDelta);
+  let html = quillSnow.getSemanticHTML(0);
+  document.querySelector("#outsnow").textContent = html;
+  document.querySelector("#outsnowc").textContent = html.replace("&nbsp;", " ");
+});
